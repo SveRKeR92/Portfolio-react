21107004381c26257154b3fcd486ffdca549a7f1
Maxime <maxime.attala@edu.devinci.fr>
2022-02-02 17:47:22 +0100

    bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alert } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      <h1>Starto</h1>
+      {      
+        [
+          'primary',
+          'secondary',
+          'success',
+          'danger',
+          'warning',
+          'info',
+          'light',
+          'dark',
+        ].map((variant, idx) => (
+          <Alert key={idx} variant={variant}>
+            This is a {variant} alert—check it out!
+          </Alert>
+        ))
+      }
     </>
   );
 }
