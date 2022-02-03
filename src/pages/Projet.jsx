c89cd9619970ffda0ba67085e8935d5a2155651e
Maxime <maxime.attala@edu.devinci.fr>
2022-02-03 21:10:19 +0100

    one project page done

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -1,13 +1,34 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar"
+import { Link, useParams } from "react-router-dom";
+import Projects from '../data/projects.json';
+
 
 
 const Projet = () => {
+
+      function GetParam(){
+            const param = useParams();
+            return param
+      }
+
+      const slug = GetParam()
+
+      const theProject = Projects.projects[slug.id-1];
+
       return(
             <>
+                  {console.log(theProject)}
                   <Navbar/>
                   <section id="projet">
-                        <h1>Le projet {this.props}</h1>
+                        <h1>{theProject.projectTitle}</h1>
+                        {theProject.projectImages.map((data, index) => 
+                              <div key={index}>
+                                    <img src={data} alt={theProject.projectTitle} />
+                              </div>
+                        )}
+                        <p>{theProject.projectDesc}</p>
+                        <Link to="/projets">Back to all projects</Link>
                   </section>
                   <Footer/>
             </>
