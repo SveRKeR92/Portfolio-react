7beee91f7270c90f95ef64f2f67ac251430ede73
Maxime <maxime.attala@edu.devinci.fr>
2022-02-04 18:01:50 +0100

    lazyload addes

--- src/routes/Rooter.jsx
+++ src/routes/Rooter.jsx
@@ -1,21 +1,27 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 
-import Home from '../pages/Home'
-import Projets from '../pages/Projets'
-import Projet from '../pages/Projet'
-import Cv from '../pages/Cv'
-import NoMatch from './NoMatch'
+import Loading from '../components/loader/Loading';
+
+//lazy import
+const Home = lazy(() => import('../pages/Home'));
+const Projets = lazy(() => import('../pages/Projets'));
+const Projet = lazy(() => import('../pages/Projet'));
+const Cv = lazy(() => import('../pages/Cv'));
+const NoMatch = lazy(() => import('./NoMatch'));
 
 const Rooter = () => (
-      <Router>
-            <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/projets" component={Projets} />
-                  <Route path="/projet/:id" component={Projet} />
-                  <Route path="/cv" component={Cv} />
-                  <Route path="*" component={NoMatch} />
-            </Switch>
-      </Router>
-)
+  <Suspense fallback={<Loading/>}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/projets" component={Projets} />
+        <Route path="/projet/:id" component={Projet} />
+        <Route path="/cv" component={Cv} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
+    </Router>
+  </Suspense>
+);
 
-export default Rooter
+export default Rooter;
